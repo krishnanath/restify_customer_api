03f5919ff25d08a6d4d4d0745ce54486a74beee5
krishnanath <isabella0484@gmail.com>
2018-11-28 21:46:20 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -16,15 +16,29 @@ exports.authenticate = (email, password) => {
     try {
       const user = await User.findOne({ email });
 
-      //Match Password
-      bcrypt.compare(password, user.password, (err, isMatch) => {
-        if (err) throw err;
-        if (! isMatch) throw 'Password did not match'; {
-          resolve(user);
-        }  
-      });
-    } catch (err) {
-      reject("Authentication Failed");
-    }
-  });
-};
+//       //Match Password
+//       bcrypt.compare(password, user.password, (err, isMatch) => {
+//         if (err) throw err;
+//         if (! isMatch) throw 'Password did not match'; {
+//           resolve(user);
+//         }  
+//       });
+//     } catch (err) {
+//       reject("Authentication Failed");
+//     }
+//   });
+// };
+
+
+// Match Password
+       bcrypt.compare(password, user.password, (err, isMatch) => {
+         if (err) throw err;
+         
+         if (!isMatch) throw 'Password did not match';
+         resolve(user);
+          });
+     } catch (err) {
+        // Email not found or password did not match
+       reject('Authentication Failed');
+     }
+   });
